0651e52c570d4f6b18bc85d4bf15036b72b60df0
Himanshu <himanshu.fernando@gmail.com>
2016-12-22 16:19:41 +0530

    2016-12-22

---
+++ app/src/main/java/emerge/lk/channelbridge/Layout/Splash.java
@@ -0,0 +1,34 @@
+package emerge.lk.channelbridge.Layout;
+
+import android.app.Activity;
+import android.app.ActivityOptions;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import emerge.lk.channelbridge.R;
+
+/**
+ * Created by Himanshu on 12/22/2016.
+ */
+
+public class Splash extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_splash);
+
+
+        new Handler().postDelayed(new Runnable() {
+            public void run() {
+                Intent i = new Intent(Splash.this,Login.class);
+                Bundle bndlanimation = ActivityOptions.makeCustomAnimation(getApplicationContext(), R.anim.animation, R.anim.animation2).toBundle();
+                finish();
+                startActivity(i, bndlanimation);
+
+            }
+        }, 3500);
+
+    }
+}
---
+++ app/src/main/java/emerge/lk/channelbridge/Layout/login.java
@@ -0,0 +1,19 @@
+package emerge.lk.channelbridge.Layout;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import emerge.lk.channelbridge.R;
+
+/**
+ * Created by Himanshu on 12/22/2016.
+ */
+
+public class Login extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_login);
+    }
+}
